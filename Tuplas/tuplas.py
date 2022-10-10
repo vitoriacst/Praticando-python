968af5647ff72dccb542cf5d423d11aabc78ffed
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-10-10 12:47:38 -0300

    docs: :memo: Tuplas em Python

---
+++ Tuplas/tuplas.py
@@ -0,0 +1,17 @@
+users = {}
+emails = ["example@email.com","name@email.com"]
+
+tupla = list(enumerate(emails))
+
+print(tupla)
+
+for key in range(0,len(tupla)):
+  print("Email", tupla[key][1])
+  users[tupla[key]]=[input("Write the name: "), input("Write the level: ")]
+
+for key, data in users.items():
+  print("users: ", key[0])
+  print("Email: ", key[1])
+  print("name: ", data[0])
+  print("level: ", data[1])
+
