d8753d08b19c36d3f0dbba5ba2ac1fc9c5f6503d
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-10-17 14:41:56 -0300

    feat: :memo: funções em python

---
+++ functions/people.py
@@ -0,0 +1,9 @@
+import area
+
+PEOPLE_PER_SQUARE_METER = 2  # numero de pessoas por metro quadrado em média
+FIELD_LENGTH = 60  # em metros
+FIELD_WIDTH = 45  # em metros
+people_at_concert = (
+    area.rectangle(FIELD_LENGTH, FIELD_WIDTH) * PEOPLE_PER_SQUARE_METER
+)
+print("Estão presentes no show aproximadamente", people_at_concert, "pessoas.")
