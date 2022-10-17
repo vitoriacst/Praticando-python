b7bde0e7e0dc50bbcb6162ed37ca6d17a44975f2
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-10-17 14:39:53 -0300

    feat: :memo: funções em python

---
+++ functions/area.py
@@ -0,0 +1,23 @@
+PI = 3.14
+
+def square(side):
+    '''Calculate area of square.'''
+    return side * side
+
+
+def rectangle(length, width):
+    '''Calculate area of rectangle.'''
+    return length * width
+
+
+def circle(radius):
+    '''Calculate area of circle.'''
+    return PI * radius * radius
+
+
+
+
+if __name__ == "__main__":
+    print("Área do quadrado:", square(10))
+    print("Área do retângulo:", rectangle(2, 2))
+    print("Área do círculo:", circle(3))
---
+++ functions/fun.py
@@ -0,0 +1,8 @@
+def sum (num1, num2):
+  resp = num1 +num2
+  return resp
+
+retorno = sum(75 ,1289)
+print(retorno)
+
+
