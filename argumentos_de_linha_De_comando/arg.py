de691b74012cd5191c47359453436a8eed5dd955
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-10-17 14:40:44 -0300

    feat: :memo: dicionários e argumentos em python

---
+++ argumentos_de_linha_De_comando/arg.py
@@ -0,0 +1,3 @@
+a = {'blue','apple'}
+a.add(1,"v")
+print(a)
---
+++ estruturas_De_Dados/dicionários.py
@@ -0,0 +1,6 @@
+meu_dicionario = {
+  'name':'vivi', 'age':11,
+}
+
+# possui chave valor
+# palavra é a chave e o significado é o valor
