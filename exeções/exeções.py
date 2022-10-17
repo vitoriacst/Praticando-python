c78f7b5c9511bb9d0ec6e5fc0c6b194cc3765f4f
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-10-17 16:53:26 -0300

    docs: :memo: exceções em python

---
+++ exeções/exeções.py
@@ -0,0 +1,12 @@
+print(10 * (1 / 0))
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# ZeroDivisionError: division by zero
+print(4 + spam * 3)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'spam' is not defined
+print('2' + 2)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: can only concatenate str (not "int") to str
