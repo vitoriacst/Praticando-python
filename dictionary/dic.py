6162d4c55e7e985fa74038186d71ad92320eb2c6
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-10-18 21:51:44 -0300

    docs: :memo: trabalhando com dicionários em python

---
+++ dictionary/dic.py
@@ -0,0 +1,10 @@
+estudantes = {'matheus': 12 , 'luan': 4 , 'ruan': 3}
+estudantes2 = {'carlos': 12 , 'lua': 4 }
+
+estudantes.update(estudantes2)
+
+print(estudantes)
+
+print(len(estudantes))
+
+print(estudantes.items)
