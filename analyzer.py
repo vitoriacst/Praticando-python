1361c0c263bffd9f0b1c8ee5aa3fb72cc8d7ba3c
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-10-18 22:47:07 -0300

    docs: :memo: trabalhando com funções em python

---
+++ analyzer.py
@@ -0,0 +1,20 @@
+
+def analyze(text:str)-> str:
+  report = ''
+  count_words = len(text.split(" "))
+  report += f"Numero de palavras = {count_words} "
+  char_counter = dict()
+  for char in text:
+    if char in char_counter:
+      char_counter[char] += 1
+    else:
+      char_counter[char] = 1
+  report += f" ocorrências por caracteres  = {char_counter}"
+
+  return report
+
+text_to_analyze = (
+  'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+)
+
+print(analyze(text_to_analyze))
